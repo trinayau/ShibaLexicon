f5e3901b358c803d8e97cbfca148f8755b82fbfd
Trina <92634994+trinayau@users.noreply.github.com>
2023-01-20 18:26:48 +0000

    add css

--- shibal-frontend/src/App.js
+++ shibal-frontend/src/App.js
@@ -1,11 +1,13 @@
-
-import './App.css';
+import "./App.css";
+import logo from "./shiba.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-     
+        <img src={logo} className="app-logo" alt="logo" />
+
+        <h1>ShibaLexicon</h1>
       </header>
     </div>
   );

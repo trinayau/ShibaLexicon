4a4690782f5dcb4b6fc2eda420d4aa5e5f30510b
Trina <92634994+trinayau@users.noreply.github.com>
2023-02-04 21:05:27 +0000

    add search bar

--- shibal-frontend/src/components/Dictionary/index.js
+++ shibal-frontend/src/components/Dictionary/index.js
@@ -1,18 +1,33 @@
+import { useState } from "react";
 import "./style.css";
 
 const Dictionary = ({ dictStatus }) => {
+
+    const [keyword, setKeyword] = useState("");
+
+    const search = (e) => {
+        e.preventDefault();
+        console.log("searching", keyword);
+    }
+
+    const handleKeywordChange = (e) => {
+        setKeyword(e.target.value);
+    }
+
   return (
     <>
       {dictStatus && (
         <div className="dictionary">
             {/* search bar */}
             <div className="search-bar">
-                <input type="text" placeholder="Search for a word" />
+                <form onSubmit={search}>
+                <input type="text" autoFocus={true} value={keyword} onChange={handleKeywordChange} placeholder="Search for a word" required />
                 <button>Search</button>
+                </form>
             </div>
 
             <div className="text">
-          <h2 className="dialog-box">Dictionary</h2>
+          <h2 className="dialog-box">ShibaLexicon</h2>
           </div>
         </div>
       )}
